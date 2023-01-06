6b2c0dfebf1c96b2e84f740e0676aa49a7a65122
Laura <laurasouzacastro1@gmail.com>
2023-01-06 15:49:46 -0300

    Recebendo dados do usuario (POST) e armazenando em variáveis globais

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
+const usuario = {username: "", avatar: ""};
+
+app.post('/sign-up', (req, res) => {
+	usuario.username = req.body.username;
+    usuario.avatar = req.body.avatar;
+    res.send(usuario);
+});
 app.listen(5000, () => {console.log("Rodando...")});
