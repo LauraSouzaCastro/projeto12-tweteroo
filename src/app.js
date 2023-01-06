45502dea5d72f538b45d49943a2cac3137d9912e
Laura <laurasouzacastro1@gmail.com>
2023-01-06 16:06:29 -0300

    Recebendo dados de um tweet, verificando usuario e armazenando tweet em variável global

--- src/app.js
+++ src/app.js
@@ -5,13 +5,23 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const usuarios = []
+const usuarios = [];
+const tweetes = [];
 
 app.post('/sign-up', (req, res) => {
-    const usuario = req.body
+    const usuario = req.body;
     usuarios.push(usuario);
-    console.log(usuarios)
     res.send("OK");
 
+});
+
+app.post('/tweets', (req, res) => {
+    const tweet = req.body;
+    if(usuarios.find(u => u.username === tweet.username)){
+        tweetes.push(tweet);
+        res.send("OK");
+    }else{
+        res.send("UNAUTHORIZED");
+    }
 });
 app.listen(5000, () => {console.log("Rodando...")});
